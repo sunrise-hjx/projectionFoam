70f16c224ec8d5f74061ab09a3744a412f494748
Sunrise <13138178+jiangxuhao@user.noreply.gitee.com>
2024-05-02 13:26:01 +0000

    .
    Signed-off-by: Sunrise <13138178+jiangxuhao@user.noreply.gitee.com>

---
+++ fvm3DprojectionRK2Foam/fvm3DprojectionRK2Foam.C
@@ -0,0 +1,154 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    icoFoam
+
+Description
+    Transient solver for incompressible, laminar flow of Newtonian fluids.
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "upwind.H"
+#include "OFstream.H"
+#include "syncTools.H"
+#include "vectorList.H"
+#include "hexMatcher.H"
+#include "prismMatcher.H"
+#include "simpleControl.H"
+#include "scalarMatrices.H"
+#include "slicedSurfaceFields.H"
+#include "volPointInterpolation.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    #include "setRootCase.H"
+    #include "createTime.H"
+    #include "createMesh.H"
+
+    simpleControl simple(mesh);
+    #include "createFields.H"
+    #include "createTimeControls.H"
+    #include "initContinuityErrs.H"
+    #include "readTimeControls.H"
+    #include "CourantNo.H"
+    #include "setInitialDeltaT.H"
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    Foam::Info << "\nStarting time loop\n" << endl;
+    while (runTime.loop())
+    {
+        #include "readTimeControls.H"
+        #include "CourantNo.H"
+        #include "setDeltaT.H"
+
+        Foam::Info<< "Time = " << runTime.timeName() << nl << endl;
+
+        volVectorField U0(U);
+        for(int r=0; r<2; r++)
+        {
+            phi = linearInterpolate(U) & mesh.Sf();
+            volTensorField gradUvol(fvc::grad(U));
+            surfaceTensorField gradUs = linearInterpolate(gradUvol);
+
+            volVectorField fl_vof = fvc::div(phi, U);
+            volVectorField fl_lap = fvc::surfaceIntegrate(nu * (mesh.Sf() & gradUs));
+            dimensionedScalar dt(word("dt"),runTime.deltaT());        
+
+            if (r == 0)
+            {
+                U = U0 - dt * fl_vof + dt * fl_lap;
+                fl_vof_0 = fl_vof;
+                fl_lap_0 = fl_lap;
+            }
+            if (r == 1)
+            {
+                U = U0 - 0.5 * dt * (fl_vof + fl_vof_0) + 0.5 * dt * (fl_lap + fl_lap_0);
+            }
+            U.correctBoundaryConditions();
+        }
+
+        phi = linearInterpolate(U) & mesh.Sf();
+        adjustPhi(phi, U, p);
+        surfaceScalarField phiU(phi);
+
+        dimensionedScalar rAUf("rAUf", runTime.deltaT());
+
+        while (simple.correctNonOrthogonal())
+        {
+            fvScalarMatrix pEqn
+            (
+                fvm::laplacian(rAUf,p) == fvc::div(phi)
+            );
+            
+            pEqn.setReference(pRefCell, pRefValue);
+
+            if (simple.finalNonOrthogonalIter())
+            {
+                pEqn.solve(mesh.solver("pFinal"));
+            }
+            else
+            {
+                pEqn.solve();
+            }
+
+            if (simple.finalNonOrthogonalIter())
+            {
+                phi -= pEqn.flux();
+            }
+        }
+
+        #include "continuityErrs.H"
+
+        U += fvc::reconstruct(phi - phiU);
+        // U += -rAUf * fvc::grad(p);
+        U.correctBoundaryConditions();     
+        // U.replace(vector::Z,0.0);
+
+        runTime.write();
+
+        Foam::Info << " Min(U) = " << min(U).value()
+             << " Max(U) = " << max(U).value()
+             << endl;
+
+        div = fvc::div(phi);
+        div.correctBoundaryConditions();
+        Info<< " Min(div) = " << min(div).value()
+            << " Max(div) = " << max(div).value()
+            << endl;
+
+        Foam::Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+            << "  ClockTime = " << runTime.elapsedClockTime() << " s"
+            << nl << endl;
+    }
+
+    Foam::Info<< "End\n" << endl;
+
+    return 0;
+}
+
+// ************************************************************************* //
